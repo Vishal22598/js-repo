17b33b7ec55e011d4d511c33d946ee9e2946f667
Vishal Gupta <51464815+Vishal22598@users.noreply.github.com>
2025-10-26 09:54:09 +0000

    changes

--- .devcontainer/01_basics/test.js
+++ .devcontainer/01_basics/test.js
@@ -1,1 +1,8 @@
 console.log("vishal gupta")
+const accId = 12345
+let accName = "Vishal"
+var branch = "Mirzamurad"
+address = "Varanasi"
+
+console.log(accId, accName, branch, address)
+console.table([accId, accName, branch, address])
