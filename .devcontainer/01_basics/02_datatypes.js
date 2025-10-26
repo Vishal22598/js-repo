c0c22681745d43f089cc14fab4d924110687aee1
Vishal Gupta <51464815+Vishal22598@users.noreply.github.com>
2025-10-26 09:53:48 +0000

    js datatypes

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; // treat all js code as new version
+//alert(3 + 3) // we are using node js not browser, so this will not work
+
+console.log(3 
+    + 3) // code readability should be high
+
+//number 2 to power 53
+//bigint
+//string => ""
+//boolean  => true/false
+//null standalone value (if we print its typeof , object)
+//undefined  => the variable which is not yet initialized
+//symbol => unique
+
+console.log("typeof undefined: " + typeof undefined)
+console.log("typeof null: " + typeof null)
